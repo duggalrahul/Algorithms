2e1f5dd368b042a44d6970a4842ac5485dfb0068
dchoudhary9 <dchoudhary9@gatech.edu>
2019-11-19 10:53:29 -0500

    LS1 modified to retirn path length

--- Code/LS1.cpp
+++ Code/LS1.cpp
@@ -8,23 +8,26 @@ using namespace std;
 
 std::vector<int> LS1(vector<tuple<int,float,float>> instance, float time, int seed){
 
-    double timeStart = clock();
-    int nVertices = instance.size();
+    unsigned int nVertices = instance.size();
+
+    // Maintain a distance matrix to keep track
     vector<vector<int>> dist(nVertices);  
-    std::vector<int> path;
+    // Store the TSP route in path
+    std::vector<int> path{seed};
+    // Store the distance of all vertices from path
+    std::vector<int> distFromPath;
+    // Boolean vector keeping track of the vertices on path
     std::vector<bool> vertexOnPath(nVertices, false);
-    int verticesOnPath = 1;
 
-    vertexOnPath[seed] = true;
-    path.push_back(seed);
+    int pathLength = 0;
 
-    for(int row = 0; row < nVertices; row++){
+    for(unsigned int row = 0; row < nVertices; row++){
         dist[row].resize(nVertices);
     }
     // Create the distance matrix
-    for(int row = 0; row < nVertices; row++)
+    for(unsigned int row = 0; row < nVertices; row++)
     {
-        for(int col = 0; col <= row; col++)
+        for(unsigned int col = 0; col <= row; col++)
         {
             float dx = get<1>(instance[row])-get<1>(instance[col]);
             float dy = get<2>(instance[row])-get<2>(instance[col]);
@@ -37,47 +40,57 @@ std::vector<int> LS1(vector<tuple<int,float,float>> instance, float time, int se
       }  
     }
 
+    double timeStart = clock();
+    distFromPath = dist[seed];
+    vertexOnPath[seed] = true;
+
+    // Add the second vertex to path
+    int newVertex = 0;
+    newVertex =  std::min_element(distFromPath.begin(),distFromPath.end())-distFromPath.begin();
+    path.push_back(newVertex);
+    pathLength += dist[seed][newVertex];
+    vertexOnPath[newVertex] = true;
+    distFromPath[newVertex] = INT_MAX;
+
+    for(unsigned int i = 0; i < nVertices;  i++){
+        if(!vertexOnPath[i])
+            distFromPath[i] = min(distFromPath[i], dist[i][newVertex]);
+    }
+
     // Start creating the Ham-Cycle
-    while(verticesOnPath < nVertices){
+    while(path.size() < nVertices){
 
         if ((clock() - timeStart) / CLOCKS_PER_SEC >= time) {
-            printf("Maximum allowed time exceeded\n");
+            printf("Maximum allowed time exceeded.\n");
             return path;
         }
 
-        int newVertex = 0;  
         // Find the next vertex to be added to the path
-        int bestdistSorted = INT_MAX;
-        for( unsigned int i = 0; i < path.size();  i++){
-            auto loc = std::min_element(dist[path[i]].begin(), dist[path[i]].end())-dist[path[i]].begin();
-            if(!vertexOnPath[loc] && dist[path[i]][loc] < bestdistSorted){
-                bestdistSorted = dist[path[i]][loc];
-                newVertex = loc;
-            } 
-        }
+        newVertex =  std::min_element(distFromPath.begin(),distFromPath.end())-distFromPath.begin();
         vertexOnPath[newVertex] = true;
-        // Remove the vertex from the set of vertices not on the path
-        for( unsigned int i = 0; i < path.size();  i++){
-            dist[path[i]][newVertex] = INT_MAX; 
+        distFromPath[newVertex] = INT_MAX;
+
+        for(unsigned int i = 0; i < nVertices;  i++){
+            if(!vertexOnPath[i])
+                distFromPath[i] = min(distFromPath[i], dist[i][newVertex]);
         }
 
         // Insert the newVertex in between an edge such that the increase in TSP length is minimum
         int insertAfter = 0;
         int df = INT_MAX;
         for( unsigned int i = 0; i < path.size()-1;  i++){
-
             int d = dist[newVertex][path[i]]+dist[newVertex][path[i+1]]-dist[path[i]][path[i+1]];
             if(d < df){
-                d = df;;
+                df = d;
                 insertAfter = i;
-
             } 
         }
+        pathLength += df;
         path.insert(path.begin()+insertAfter+1, newVertex);
 
-        verticesOnPath++;
     }
 
+    path.push_back(pathLength);
     // Print out the solution
     for(unsigned int i = 0; i < path.size(); i++){
         std::cout<<path[i]<<" ";
