ec8a970724a797a5174802e247dbac2fbbf89255
Vasconcelos Da Senhora, Fernando <fernandosenhora@gatech.edu>
2019-12-03 20:59:12 -0500

    documentation

--- Code/auxiliar_func.cpp
+++ Code/auxiliar_func.cpp
@@ -1,120 +1,81 @@
 #include "auxiliar_func.h"
 
 
-int inf = 100000000000;
-
-vector<vector<int>> compute_dist_matrix(vector<double> coord_x, vector<double> coord_y)
-{
-
-	int n = coord_x.size();
-	vector<vector<int>> distances;
-
-
-	distances.resize(n);
-
-
-	int max_dist = 0;
-	for (int row = 0; row<n; row++)
-	{
-		for (int col = 0; col < n; col++)
-		{
-			
-			distances[row].push_back(int(sqrt(pow(coord_x[row] - coord_x[col], 2) + pow(coord_y[row] - coord_y[col], 2)) + 0.50000000001));
-			
-			if (distances[row][col]>max_dist)
-				max_dist = distances[row][col] + 1;
-		}
-		
-	}
-	printf("max distanace %d\n", max_dist);
-	max_dist *= n;
-	inf = max_dist;
-
-	for (int row = 0; row < n; row++)
-	{
-		distances[row][row] = max_dist;
-	}
-	printf("inf distanace %d\n", inf);
-
-	return distances;
-}
-
-
-vector<vector<int>> compute_dist_matrix(vector<float> coord_x, vector<float> coord_y)
-{
-
-	int n = coord_x.size();
-	vector<vector<int>> distances;
-
-
-	distances.resize(n);
-
-
-	int max_dist = 0;
-	for (int row = 0; row<n; row++)
-	{
-		for (int col = 0; col < n; col++)
-		{
-
-			distances[row].push_back(int(sqrt(pow(coord_x[row] - coord_x[col], 2) + pow(coord_y[row] - coord_y[col], 2)) + 0.50000000001));
-
-			if (distances[row][col]>max_dist)
-				max_dist = distances[row][col] + 1;
-		}
-
-	}
-	printf("max distanace %d\n", max_dist);
-	max_dist *= n;
-	inf = max_dist;
-
-	for (int row = 0; row < n; row++)
-	{
-		distances[row][row] = max_dist;
-	}
-	printf("inf distanace %d\n", inf);
-
-	return distances;
-}
-
-
+/*
+**********************************************************************************************************************************
+* DESCRIPTION:
+* Computes the distance matrix for the instance of the problem.
+**********************************************************************************************************************************
+* INPUTS:
+* vector<tuple<int, double, double>>: Each tuple represents one city of the instance, 
+ the tuple has the form: <index, x coordinate, y coordinate>
+**********************************************************************************************************************************
+* OUTPUT:
+* Return: The distance matrix, entry (i,j) of matrix represents the distance between cities i and j.
+*/
 vector<vector<int>> compute_dist_matrix(vector<tuple<int, double, double>> instance)
 {
-
+    // Get instance size
 	int n = instance.size();
+    
+    // Initialize distance matrix
 	vector<vector<int>> distances;
-
-	distances.resize(n);
-	
+	distances.resize(n);	
 
 	int max_dist = 0;
+    
+    // Run through the cities and compute pairwise distance
 	for (int row = 0; row<n; row++)
 	{
 		for (int col = 0; col < n; col++)
 		{
-
+            // Computes Euclidean distance rounded to the nearest int.
 			distances[row].push_back(int(sqrt(pow(std::get<1>(instance[row]) - std::get<1>(instance[col]), 2)
 				+ pow(std::get<2>(instance[row]) - std::get<2>(instance[col]), 2)) + 0.50000000001));
 
+            // Compute the largest distance
 			if (distances[row][col]>max_dist)
 				max_dist = distances[row][col] + 1;
 		}
 
 	}
-	printf("max distanace %d\n", max_dist);
+	
+    // Update the inf value
 	max_dist *= n;
 	inf = max_dist;
 
+    // Set diagonal of distance matrix to inf
 	for (int row = 0; row < n; row++)
 	{
 		distances[row][row] = max_dist;
 	}
-	printf("inf distanace %d\n", inf);
+	
 
 	return distances;
 }
 
-
-int output_solution(string filename, string method, float cutoff, int random_seed,  int best, vector<int> best_sol)
+/*
+**********************************************************************************************************************************
+* DESCRIPTION:
+* Creates the solution file.
+**********************************************************************************************************************************
+* INPUTS:
+* string filename: filename with path of the input instance
+*
+* string method: Algorithm used for the solution.
+*
+* float cutoff: Cutoff time used with the algorithm.
+*
+* int random_seed: Random seed used for the algorithm.
+*
+* int best: Best solution found.
+*
+* vector<int> best_sol: Best solution found
+**********************************************************************************************************************************
+* OUTPUT:
+* Return: 0 ( no meaning )
+*/
+int output_solution(string filename, string method, float cutoff, int random_seed, int best, vector<int> best_sol)
 {
 	
 	// Remove extension if present.
@@ -144,7 +105,25 @@ int output_solution(string filename, string method, float cutoff, int random_see
 	return 0;
 }
 
-
+/*
+**********************************************************************************************************************************
+* DESCRIPTION:
+* Creates the solution file.
+**********************************************************************************************************************************
+* INPUTS:
+* string filename: filename with path of the input instance
+*
+* string method: Algorithm used for the solution.
+*
+* float cutoff: Cutoff time used with the algorithm.
+*
+* int best: Best solution found.
+*
+* vector<int> best_sol: Best solution found
+**********************************************************************************************************************************
+* OUTPUT:
+* Return: 0 ( no meaning )
+*/
 int output_solution(string filename, string method, float cutoff, int best, vector<int> best_sol)
 {
 	
@@ -177,7 +156,25 @@ int output_solution(string filename, string method, float cutoff, int best, vect
 }
 
 
-
+/*
+**********************************************************************************************************************************
+* DESCRIPTION:
+* Creates the trace file.
+**********************************************************************************************************************************
+* INPUTS:
+* string filename: filename with path of the input instance
+*
+* string method: Algorithm used for the solution.
+*
+* float cutoff: Cutoff time used with the algorithm.
+*
+* int random_seed: Random seed used for the algorithm.
+*
+* vector<tuple<int, double>> trace: Trace of the algorithm. Tuple of the form <solution value, time> 
+**********************************************************************************************************************************
+* OUTPUT:
+* Return: 0 ( no meaning )
+*/
 int output_trace(string filename, string method, float cutoff, int random_seed, vector<tuple<int, double>> trace)
 {
 
@@ -211,7 +208,23 @@ int output_trace(string filename, string method, float cutoff, int random_seed, 
 	return 0;
 }
 
-
+/*
+**********************************************************************************************************************************
+* DESCRIPTION:
+* Creates the trace file.
+**********************************************************************************************************************************
+* INPUTS:
+* string filename: filename with path of the input instance
+*
+* string method: Algorithm used for the solution.
+*
+* float cutoff: Cutoff time used with the algorithm.
+*
+* vector<tuple<int, double>> trace: Trace of the algorithm. Tuple of the form <solution value, time> 
+**********************************************************************************************************************************
+* OUTPUT:
+* Return: 0 ( no meaning )
+*/
 int output_trace(string filename, string method, float cutoff, vector<tuple<int, double>> trace)
 {
 
